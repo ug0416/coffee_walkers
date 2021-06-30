e157b3f9a8358d761ac459f9c0e2766d4e905006
20170401_bazz <yuji.oda@bazzstore.com>
2021-06-30 18:15:20 +0900

    change homepage style

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,32 +1,117 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { Location } from '@reach/router'
+import qs from 'qs'
 
 import PageHeader from '../components/PageHeader'
-import Content from '../components/Content'
+import PostSection from '../components/PostSection'
+import PostCategoriesNav from '../components/PostCategoriesNav'
 import Layout from '../components/Layout'
 
-// Export Template for use in CMS preview
-export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
-  <main className="Home">
-    <PageHeader
-      large
-      title={title}
-      subtitle={subtitle}
-      backgroundImage={featuredImage}
-    />
+/**
+ * Filter posts by date. Feature dates will be fitered
+ * When used, make sure you run a cronejob each day to show schaduled content. See docs
+ *
+ * @param {posts} object
+ */
+export const byDate = posts => {
+  const now = Date.now()
+  return posts.filter(post => Date.parse(post.date) <= now)
+}
 
-    <section className="section">
-      <div className="container">
-        <Content source={body} />
-      </div>
-    </section>
-  </main>
+/**
+ * filter posts by category.
+ *
+ * @param {posts} object
+ * @param {title} string
+ * @param {contentType} string
+ */
+export const byCategory = (posts, title, contentType) => {
+  const isCategory = contentType === 'postCategories'
+  const byCategory = post =>
+    post.categories &&
+    post.categories.filter(cat => cat.category === title).length
+  return isCategory ? posts.filter(byCategory) : posts
+}
+
+// Export Template for use in CMS preview
+export const HomePageTemplate = ({
+  title,
+  subtitle,
+  featuredImage,
+  posts = [],
+  postCategories = [],
+  enableSearch = true,
+  contentType
+}) => (
+  <Location>
+    {({ location }) => {
+      let filteredPosts =
+        posts && !!posts.length
+          ? byCategory(byDate(posts), title, contentType)
+          : []
+
+      let queryObj = location.search.replace('?', '')
+      queryObj = qs.parse(queryObj)
+
+      if (enableSearch && queryObj.s) {
+        const searchTerm = queryObj.s.toLowerCase()
+        filteredPosts = filteredPosts.filter(post =>
+          post.frontmatter.title.toLowerCase().includes(searchTerm)
+        )
+      }
+
+      return (
+        <main className="Blog">
+          <PageHeader
+            title={title}
+            subtitle={subtitle}
+            backgroundImage={featuredImage}
+          />
+
+          {!!postCategories.length && (
+            <section className="section thin">
+              <div className="container">
+                <PostCategoriesNav enableSearch categories={postCategories} />
+              </div>
+            </section>
+          )}
+
+          {!!posts.length && (
+            <section className="section">
+              <div className="container">
+                <PostSection posts={filteredPosts} />
+              </div>
+            </section>
+          )}
+        </main>
+      )
+    }}
+  </Location>
 )
 
 // Export Default HomePage for front-end
-const HomePage = ({ data: { page } }) => (
-  <Layout meta={page.frontmatter.meta || false}>
-    <HomePageTemplate {...page} {...page.frontmatter} body={page.html} />
+const HomePage = ({ data: { page, posts, postCategories } }) => (
+  
+  <Layout
+    meta={page.frontmatter.meta || false}
+    title={page.frontmatter.title || false}
+  >
+    <HomePageTemplate
+      {...page}
+      {...page.fields}
+      {...page.frontmatter}
+      posts={posts.edges.map(post => ({
+        ...post.node,
+        ...post.node.frontmatter,
+        ...post.node.fields
+      }))}
+      postCategories={postCategories.edges.map(post => ({
+        ...post.node,
+        ...post.node.frontmatter,
+        ...post.node.fields
+      }))}
+    />
   </Layout>
 )
 
@@ -40,11 +125,52 @@ export const pageQuery = graphql`
   query HomePage($id: String!) {
     page: markdownRemark(id: { eq: $id }) {
       ...Meta
-      html
+      fields {
+        contentType
+      }
       frontmatter {
         title
+        excerpt
+        template
         subtitle
         featuredImage
+      }
+    }
+
+    posts: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "posts" } } }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+            categories {
+              category
+            }
+            featuredImage
+          }
+        }
+      }
+    }
+    postCategories: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "postCategories" } } }
+      sort: { order: ASC, fields: [frontmatter___title] }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
       }
     }
   }
