3fcaf134510ce4a48dc0ab9fde6da790f8a5f9d3
20170401_bazz <yuji.oda@bazzstore.com>
2021-08-25 17:13:07 +0900

    add gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,32 @@ module.exports = {
         icon: `${__dirname}/static/fabicon-logo.png` // This path is relative to the root of the site.
       }
     },
+    
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`
+          },
+          {
+            // uploadcare
+            urlPattern: /^https:\/\/ucarecdn.com\/[-a-zA-Z0-9@:%_\+.~#?&//=]*?\/10x\//,
+            handler: `staleWhileRevalidate`
+          }
+        ],
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    },
 
     // Add static assets before markdown files
     {
